a78725a65f91136032b5d13af5e98cf949e2d7ed
DragonMaster14545 <philippschwarzenbrunner@gmail.com>
2024-03-19 10:58:50 +0100

    Update .gitignore

--- kubejs/server_scripts/fantasy_craft.js
+++ kubejs/server_scripts/fantasy_craft.js
@@ -154,7 +154,7 @@ function setUpPlayer(e,player) {
     }
     playerSaveData.servants = []
     playerPersistantData.initiated = true
-    player.teleportTo(e.server.getLevel('fantasy_craft:void_dimension'), 0, 0, 0, [], player.yaw, player.pitch)
+    player.teleportTo(e.server.getLevel('fantasy_craft:void_dimension1'), 0, 0, 0, [], player.yaw, player.pitch)
     e.server.runCommandSilent('/execute in fantasy_craft:void_dimension run photon fx kubejs:player_spawn block 0 0 0')
     e.server.runCommandSilent(`/openguiscreen greeting_dialogue ${player.username}`)
     }
